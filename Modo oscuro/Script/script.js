08f464302102264e0f6e8aad8fa452e07a9b8dbe
alfonsaco <alfonso.rincondev@gmail.com>
2024-06-23 22:18:19 +0200

    Modo oscuro terminado

--- Modo oscuro/Script/script.js
+++ Modo oscuro/Script/script.js
@@ -1,11 +1,30 @@
-const divIconos=document.querySelector(".div-iconos");
-const sol=document.getElementById("sun");
-const luna=document.getElementById("moon");
+document.addEventListener("DOMContentLoaded", function() {
+    const divIconos=document.querySelector(".div-iconos");
+    const sol=document.getElementById("sun");
+    const luna=document.getElementById("moon");
 
-divIconos.addEventListener("click", function() {
-    // Animación luna y sol
-    luna.classList.toggle("ocultar-luna");
-    sol.classList.toggle("mostrar-sol");
-    // Cambiar modo
-    document.body.classList.toggle("dark-mode");
+
+    divIconos.addEventListener("click", function() {
+        // Animación luna y sol
+        luna.classList.toggle("ocultar-luna");
+        sol.classList.toggle("mostrar-sol");
+        // Cambiar modo
+        document.body.classList.toggle("dark-mode");
+
+        // Condiciones para que en el caso de que se haya pulsado en el botón (se agrega la clase dark-mode al body),
+        // se ponga por defecto esa clase al entrar en la página
+        if(document.body.classList.contains("dark-mode")){
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+
+    // En caso de que esté habilitado, se añaden las clases
+    if(localStorage.getItem("darkMode") == "enabled"){
+        luna.classList.add("ocultar-luna");
+        sol.classList.add("mostrar-sol");
+        // Cambiar modo
+        document.body.classList.add("dark-mode");
+    }
 });
