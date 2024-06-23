2cf2a4bc13225829c876a403558d7b87814deff0
alfonsaco <alfonso.rincondev@gmail.com>
2024-06-23 21:49:43 +0200

    Terminada la funcionalida del modo oscuro

--- Modo oscuro/Script/script.js
+++ Modo oscuro/Script/script.js
@@ -3,6 +3,9 @@ const sol=document.getElementById("sun");
 const luna=document.getElementById("moon");
 
 divIconos.addEventListener("click", function() {
+    // Animación luna y sol
     luna.classList.toggle("ocultar-luna");
     sol.classList.toggle("mostrar-sol");
+    // Cambiar modo
+    document.body.classList.toggle("dark-mode");
 });
