a6f2ddf5f01f923ab92e75bb01ba57f9fc135bab
alfonsaco <alfonso.rincondev@gmail.com>
2024-07-13 20:58:59 +0200

    JavaScript y HTML terminados

--- Reloj Analógico/Script/script.js
+++ Reloj Analógico/Script/script.js
@@ -1,11 +1,21 @@
 const segundos=document.getElementById("segundos");
-
+const minutos=document.getElementById("minutos");
+const horas=document.getElementById("horas");
+// Reloj
 setInterval(() => {
     let hoy=new Date();
 
-    let gradosSeg=hoy.getSeconds()*6;
-
     segundos.style.rotate=(hoy.getSeconds()*6)+'deg';
-
+    minutos.style.rotate=(hoy.getMinutes()*6)+'deg';
+    horas.style.rotate=(hoy.getHours()*30)+'deg';
 
 }, 1000);
+
+// For para crear los números
+const cifras=document.getElementById("cifras");
+
+let grados=0;
+for (let i = 0; i < 12; i++) {
+    cifras.innerHTML+='<div class="numero" style="rotate: '+grados+'deg"</div>';
+    grados+=30;
+}
