afed19f84cd9f9e036887e0dbe5c7f4d62e8fffa
alfonsaco <alfonso.rincondev@gmail.com>
2024-08-30 19:35:47 +0200

    Se termina la funcionalidad del juego CPS

--- CPS (Clicks Per Second)/Script/script.js
+++ CPS (Clicks Per Second)/Script/script.js
@@ -7,19 +7,43 @@ const mensaje=document.getElementById("mensaje");
 let cont=0;
 let estado=false;
 
+const segundos=document.querySelectorAll("#segundos div");
+let tiempo=10;
+let booleanTiempo=true;
+console.log(segundos)
+
+// Se usarán 10s por defecto
+segundos[2].classList.add("segundo-activo");
+
+segundos.forEach(seg => {
+    seg.addEventListener("click", function() {
+        segundos.forEach(s => {
+            s.classList.remove("segundo-activo");
+        });
+        seg.classList.add("segundo-activo");
+        
+        if(booleanTiempo == true) {
+            tiempo=seg.textContent;
+        }
+    })
+});
+
+
 // Juego contador clicks
 area.addEventListener("click", function() {
+    booleanTiempo=false;
     if(cont == 0) {
         estado=true;
         cont++;
         contador.textContent=cont;
         setTimeout(() => {
             estado=false;
+            booleanTiempo=true;
 
             mensaje.classList.add("aparecer-mensaje");
             mensaje.querySelector("p").innerHTML=`TOTAL CLICKS: <span>${cont}</span> <br>
-                                                  CLICKS POR SEGUNDO: <span>${cont/5}</span>`;
-        }, 100);
+                                                  CLICKS POR SEGUNDO: <span>${cont/tiempo}</span>`;
+        }, (tiempo*1000));
     } else {
         if(estado != false) {
             cont++;
