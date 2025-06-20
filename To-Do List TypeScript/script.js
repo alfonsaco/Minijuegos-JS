b7f633b22e78946fa010a2912aac0fd3f72fd04f
alfonsaco <alfonso.rincondev@gmail.com>
2025-06-21 01:57:20 +0200

    To-Do List de TypeScript terminada

--- To-Do List TypeScript/script.js
+++ To-Do List TypeScript/script.js
@@ -1,4 +1,6 @@
 "use strict";
+let tareas = [];
+// Abrir o cerrar formulario
 const botonAbrirDialogo = document.querySelector(".boton-nueva-tarea");
 const dialogo = document.querySelector(".dialogo-tarea");
 const fondoNegro = document.querySelector(".fondo-negro");
@@ -38,4 +40,64 @@ if (fondoNegro) {
             fondoNegro.style.visibility = 'hidden';
         }
     });
+}
+// Botón guardar
+const btnGuardar = document.querySelector(".btnGuardar");
+const descripcion = document.getElementById("descripcion");
+// Colores
+let color = "";
+const colores = document.querySelectorAll(".colores > div");
+// Colorear borde
+function borrarColores() {
+    colores.forEach(c1 => {
+        c1.style.border = '1px solid #474747';
+    });
+}
+if (colores) {
+    colores.forEach(c => {
+        c.addEventListener("click", (event) => {
+            var _a;
+            borrarColores();
+            c.style.border = '2px solid #474747';
+            if (c) {
+                color = (_a = c.getAttribute("data-color")) !== null && _a !== void 0 ? _a : "";
+            }
+        });
+    });
+}
+if (btnGuardar) {
+    btnGuardar.addEventListener("click", (event) => {
+        if (descripcion) {
+            let textoDescripcion = descripcion.value;
+            if (textoDescripcion != "" && textoDescripcion != null && color != "") {
+                tareas.push({ nombre: textoDescripcion, color: color });
+                nuevaTarea(textoDescripcion, color);
+                color = "";
+                descripcion.value = "";
+                borrarColores();
+                if (dialogo) {
+                    dialogo.style.transform = 'translate(-50%, -50%) scale(0)';
+                    dialogo.style.visibility = 'hidden';
+                }
+                if (fondoNegro) {
+                    fondoNegro.style.opacity = '0';
+                    fondoNegro.style.visibility = 'hidden';
+                }
+            }
+        }
+    });
+}
+function nuevaTarea(nombre, color) {
+    const div = document.createElement("div");
+    div.className = "tarea-nueva";
+    div.style.backgroundColor = color;
+    div.innerHTML = `
+        <p>${nombre}</p>
+        <i class="fa-solid fa-trash"></i>`;
+    const borrarTarea = div.querySelector("i");
+    borrarTarea === null || borrarTarea === void 0 ? void 0 : borrarTarea.addEventListener("click", () => {
+        div.remove();
+    });
+    const contenedorTareas = document.querySelector(".contenedor > div");
+    contenedorTareas === null || contenedorTareas === void 0 ? void 0 : contenedorTareas.appendChild(div);
 }
--- To-Do List TypeScript/script.ts
+++ To-Do List TypeScript/script.ts
@@ -1,3 +1,11 @@
+// Lista de tareas
+type Tarea = {
+    nombre: string,
+    color: string
+}
+let tareas: Tarea[] = [];
+
+// Abrir o cerrar formulario
 const botonAbrirDialogo = document.querySelector(".boton-nueva-tarea") as HTMLDivElement | null;
 const dialogo = document.querySelector(".dialogo-tarea") as HTMLDivElement | null;
 const fondoNegro = document.querySelector(".fondo-negro") as HTMLDivElement | null;
@@ -42,4 +50,76 @@ if(fondoNegro) {
             fondoNegro.style.visibility='hidden';
         }
     });
+}
+
+// Botón guardar
+const btnGuardar = document.querySelector(".btnGuardar") as HTMLDivElement | null;
+const descripcion = document.getElementById("descripcion") as HTMLInputElement | null;
+
+// Colores
+let color: string = "";
+const colores = document.querySelectorAll<HTMLDivElement>(".colores > div");
+
+// Colorear borde
+function borrarColores() : void {
+    colores.forEach(c1 => {
+        c1.style.border='1px solid #474747';
+    });
+}
+if(colores) {
+    colores.forEach(c => {
+        c.addEventListener("click", (event: MouseEvent) => {
+            borrarColores();
+
+            c.style.border='2px solid #474747';
+
+            if(c) {
+                color = c.getAttribute("data-color") ?? "";
+            }
+        });
+    });
+}
+
+if(btnGuardar) {
+    btnGuardar.addEventListener("click", (event: MouseEvent) => {
+        if(descripcion) {
+            let textoDescripcion: string = descripcion.value;
+            
+            if(textoDescripcion != "" && textoDescripcion != null && color != "") {
+                tareas.push({nombre: textoDescripcion, color: color});
+                nuevaTarea(textoDescripcion, color);
+                
+                color="";
+                descripcion.value="";
+                borrarColores();
+
+                if(dialogo) {
+                    dialogo.style.transform = 'translate(-50%, -50%) scale(0)'
+                    dialogo.style.visibility='hidden';
+                }
+
+                if(fondoNegro) {
+                    fondoNegro.style.opacity='0';
+                    fondoNegro.style.visibility='hidden';
+                }
+            }
+        }
+    });
+}
+
+function nuevaTarea(nombre: string, color: string) : void {
+    const div = document.createElement("div");
+    div.className = "tarea-nueva";
+    div.style.backgroundColor=color;
+    div.innerHTML = `
+        <p>${nombre}</p>
+        <i class="fa-solid fa-trash"></i>`
+
+    const borrarTarea = div.querySelector("i");
+    borrarTarea?.addEventListener("click", () => {
+        div.remove();
+    })
+
+    const contenedorTareas=document.querySelector(".contenedor > div") as HTMLDivElement | null;
+    contenedorTareas?.appendChild(div);
 }
